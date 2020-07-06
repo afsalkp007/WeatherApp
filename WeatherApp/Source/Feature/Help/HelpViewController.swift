bfdd8a775fc2cca5f69233c2c56aec7b8ae02608
MohammedAfsal <afsalkp.mohammed@gmail.com>
2020-07-06 16:38:41 +0400

    added loader for webview

--- WeatherApp/Source/Feature/Help/HelpViewController.swift
+++ WeatherApp/Source/Feature/Help/HelpViewController.swift
@@ -7,10 +7,12 @@
 //
 
 import UIKit
+import WebKit
 
 class HelpViewController: UIViewController {
     
     @IBOutlet weak var webView: UIWebView!
+    private let activityIndicator = UIActivityIndicatorView(style: .gray)
     override func viewDidLoad() {
         super.viewDidLoad()
         title = TitleManager.about.localized
@@ -22,10 +24,36 @@ class HelpViewController: UIViewController {
             target: self,
             action: #selector(Self.dismissButtonTapped))
         
+        view.addSubview(activityIndicator)
+        NSLayoutConstraint.activate([
+            activityIndicator.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            activityIndicator.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+        
+        webView.delegate = self
         webView.loadHTMLString(Constants.html.text.htmlText, baseURL: nil)
     }
     
     @objc private func dismissButtonTapped(_ sender: UIBarButtonItem) {
         self.dismiss(animated: true)
+    }
+    
+    private func showActivityIndicator(show: Bool) {
+        if show {
+            activityIndicator.startAnimating()
+        } else {
+            activityIndicator.stopAnimating()
+        }
+    }
+}
+
+extension HelpViewController:  UIWebViewDelegate {
+    
+    func webViewDidStartLoad(_ webView: UIWebView) {
+        showActivityIndicator(show: true)
+    }
+    
+    func webViewDidFinishLoad(_ webView: UIWebView) {
+        showActivityIndicator(show: false)
     }
 }
